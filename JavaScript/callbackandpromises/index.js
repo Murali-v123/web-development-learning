a6e06a74e57996143e806e7f0153415fb3f88550
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-09-10 17:59:10 +0530

    Modified the fuctions

--- JavaScript/callbackandpromises/index.js
+++ JavaScript/callbackandpromises/index.js
@@ -1,26 +1,33 @@
 // 1. Asynchronous execution demonstration
 // alert("hello")
-console.log("sentence 1");
-console.log("sentence 2");
+// console.log("sentence 1");
+// console.log("sentence 2");
 
-setTimeout(() => {
-    console.log("Under timeout function");
-}, 1);
+// setTimeout(() => {
+//     console.log("Under timeout function");
+// }, 1);
 
 
 const fn = () => { 
-    console.log("hello world");
+    console.log("hello world its me when the script loadds");
 }
-const callback = (arg,fn) =>{
+const callback = (arg) =>{
     console.log(arg);
+    alert(arg)
     fn()
 }
 
 const loadscript = (src,callback) =>{
-    let sc=document.createElement("script")
-    sc.src=src
-    sc.onload=callback("hello",fn) //or
-    // sc.onload=()=>callback("sucessful",fn) 
+    // let sc=document.createElement("script")
+    // sc.src=src
+    // sc.onload=callback("hello",fn)//or
+    // // sc.onload=()=>callback("sucessful",fn) 
+    // console.log("The Script has been loading sucessfully");
+    // alert("the script is : "+src)
+    // document.head.append(sc)
+    let sr=document.createElement("script")
+    sr.src=src
+    sr.onload=callback("hey boy")
     document.head.append(sc)
 }
 
