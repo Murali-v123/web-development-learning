5289a24139517ab94514c4a99c5b2449dc2f29e2
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-09-26 16:06:49 +0530

    Node Modules

---
+++ Node/1stfile/index.js
@@ -0,0 +1,16 @@
+// console.log("Hey Bhaii");
+// console.log("Wassup BhAAi")
+const fs=require("fs");
+
+// fs.writeFile("Hello.txt",("Hello world"),(err)=>{
+//     if(err) throw err
+//     console.log("The file has sucessfuly created");
+    
+// })
+
+fs.readFile("Hello.txt","utf-8",(err,data)=>{
+    if(err) throw err
+    // console.log(data.toString());
+    console.log(data);
+    // console.log(data.toJSON());
+});
--- Node/writeandreadop/script.js
+++ Node/writeandreadop/script.js
@@ -1,16 +1,29 @@
 // console.log("Hey Bhaii");
 // console.log("Wassup BhAAi")
-const fs=require("fs");
+// const fs=require("fs");
+import fs from "fs"
 
-fs.writeFile("Hello.txt",("Hello world"),(err)=>{
+fs.writeFile("Hello1.txt",("Hello world, hey wassup how are you guys doing"),(err)=>{
     if(err) throw err
     console.log("The file has sucessfuly created");
     
-})
-
-fs.readFile("Hello.txt","utf-8",(err,data)=>{
-    if(err) throw err
-    // console.log(data.toString());
-    console.log(data);
-    // console.log(data.toJSON());
+    fs.readFile("Hello1.txt","utf-8",(err,data)=>{
+        if(err) throw err
+        // console.log(data.toString());
+        console.log(data);
+        // console.log(data.toJSON());
+    })
 });
+
+// fs.writeFile("Hello.txt",("Hello world"),(err)=>{
+//     if(err) throw err
+//     console.log("The file has sucessfuly created");
+    
+// })
+
+// fs.readFile("Hello.txt","utf-8",(err,data)=>{
+//     if(err) throw err
+//     // console.log(data.toString());
+//     console.log(data);
+//     // console.log(data.toJSON());
+// });
